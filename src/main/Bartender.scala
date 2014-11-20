3e43c547179f8f8d9160692b5c706f7a5a602ace
Tuomas Anttila <tt_anttila@hotmail.com>
2014-11-20 15:17:37 +0200

    kopsasin tekstiUI:n mutten muokannut, tein kartan, lisäsin alueet Adventureen

--- src/main/Bartender.scala
+++ src/main/Bartender.scala
@@ -3,7 +3,7 @@ package main
 class Bartender(name: String, startingArea: Area, initialDrunkenness: Int, initialStress: Int, sex: Gender)
       extends NPC(name, startingArea, initialDrunkenness, initialStress, sex) {
   
-  def act = { //pysyy paikoillaan, t‰ydent‰‰ juomavarastoaan, juo (jos stressi‰ paljon) tai h‰lytt‰‰ poliisin
+  def act = { // t‰ydent‰‰ juomavarastoaan (jos tyhj‰), juo (jos stressi‰ paljon), katselee ymp‰rilleen tai h‰lytt‰‰ poliisin
     
   }
   
